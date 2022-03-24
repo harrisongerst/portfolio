c1ebf18c1b2b970c42e3889736f1ba45a4481529
harrisongers <hjgerstenlauer@gmail.com>
2022-03-24 16:12:29 -0400

    add beginning navbar, change favicon and title

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bulma/css/bulma.min.css';
+import { Navbar } from 'react-bulma-components';
+
+export default function Header() {
+  return (
+    <div>
+        <Navbar>
+            <Navbar.Item> Check </Navbar.Item>
+        </Navbar>
+    </div>
+  )
+}
