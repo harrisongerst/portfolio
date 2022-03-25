955f020cc240d29b3565932d86184ffbaa1db66b
harrisongers <hjgerstenlauer@gmail.com>
2022-03-25 14:32:44 -0400

    add basic structure for projects page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,49 @@
 import React from 'react'
 import Header from '../components/Header'
+import {Card, Media, Section, Content, Heading, Image } from 'react-bulma-components'
 
 export default function Projects() {
   return (
     <div>
       <Header/>
-      Projects</div>
+      <Card>
+    <Card.Content>
+      <Media>
+        <Media.Item>
+          <Heading size={4}>Brew School</Heading>
+          <Heading subtitle size={6}>
+            Built with Node.js, React, Express, MongoDB, and BCrypt
+          </Heading>
+        </Media.Item>
+      </Media>
+      <Content>
+       And here is where I will add an image above and a short description of the project{' '}
+      </Content>
+    </Card.Content>
+    <Card.Footer>
+        <Card.Footer.Item renderAs="a" href="https://github.com/harrisongerst/brewSchool-frontend">Github</Card.Footer.Item>
+        <Card.Footer.Item renderAs="a" href="https://dev--brew-school.netlify.app/">Live</Card.Footer.Item>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Content>
+      <Media>
+        <Media.Item>
+          <Heading size={4}>A second project</Heading>
+          <Heading subtitle size={6}>
+            Built with Node.js, React, Express, MongoDB, and BCrypt
+          </Heading>
+        </Media.Item>
+      </Media>
+      <Content>
+       And here is where I will add an image above and a short description of the project{' '}
+      </Content>
+    </Card.Content>
+    <Card.Footer>
+        <Card.Footer.Item renderAs="a" href="https://github.com/harrisongerst/brewSchool-frontend">Github</Card.Footer.Item>
+        <Card.Footer.Item renderAs="a" href="https://dev--brew-school.netlify.app/">Live</Card.Footer.Item>
+    </Card.Footer>
+  </Card>
+    </div>
   )
 }
